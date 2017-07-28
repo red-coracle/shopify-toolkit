c8b6c11752e02cc2d0375d0b3baa5cc5ff966bb4
red-coracle <red-coracle@users.noreply.github.com>
2017-07-27 20:17:24 -0400

    Download product images and info to subfolder

--- product.py
+++ product.py
@@ -1,10 +1,24 @@
 import csv
 import requests
 from config import STORE_URL
+from io import open as iopen
+from os import makedirs
+from os.path import exists
+from urllib.parse import urlsplit
 
 
 def get_product(id):
     return requests.get(f'{STORE_URL}/products/{id}.json').json()
+
+
+def save_image(image_url, directory=''):
+    filename = urlsplit(image_url)[2].split('/')[-1]
+    r = requests.get(image_url)
+    if r.status_code == 200:
+        if len(directory) > 0 and not exists(directory):
+            makedirs(directory)
+        with iopen(f'{directory}{filename}', 'wb') as file:
+            file.write(r.content)
 
 
 def list_to_dict(list, key):
@@ -14,7 +28,11 @@ def list_to_dict(list, key):
 if __name__ == '__main__':
     product = get_product(input('Product ID: '))['product']
     images = list_to_dict(product['images'], 'id')
-    with open(f'{product["id"]}.csv', 'w') as csvfile:
+
+    for image in product['images']:
+        save_image(image['src'], f'{image["product_id"]}/')
+
+    with open(f'{product["id"]}/{product["id"]}.csv', 'w') as csvfile:
         fieldnames = ['title', 'item_sku', 'main_image_url', 'option1', 'option2',
                       'price']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
